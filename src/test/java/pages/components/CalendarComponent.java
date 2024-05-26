0a4273b01dc5fd7cd6852a9aad5628a64cf9efff
Daria Surkova <moe_more@list.ru>
2024-05-26 19:51:47 +0700

    test fix, date sometimes is not visible

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -13,6 +13,7 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
+        dayLocator(day).scrollIntoView(true);
         dayLocator(day).click();
     }
 }
